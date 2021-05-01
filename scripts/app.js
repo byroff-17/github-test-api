aa1480a78f918d4feeff33a4cbeda27834ed1b7d
Byron Monroy <bslab17@gmail.com>
2021-04-30 22:19:20 -0500

    Update: Added base project with basic template

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular
+    .module('github-test-api', [
+        'ui.router', 'ngRoute', 'ui.bootstrap'
+    ])
+    .config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
+        $urlRouterProvider.otherwise(function ($injector) {
+            var $state = $injector.get("$state");
+            $state.go("home");
+        });
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'templates/home.html',
+                controller: 'homeCtrl'
+            })
+    }])
---
+++ scripts/controllers/homeCtrl.js
@@ -0,0 +1,9 @@
+angular
+.module('github-test-api')
+.controller('homeCtrl', ['$scope', function($scope){
+	$scope.title="Home Welcome Page";
+
+	
+	$(document).ready(function() {
+	});
+}]);
